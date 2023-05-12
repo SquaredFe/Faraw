6fb5929e5d10ed153723fac604218d7f2e931046
FeFe <90733495+fefedevv@users.noreply.github.com>
2023-05-11 23:50:42 -0300

    Test
    fefe

---
+++ main/main.py
@@ -0,0 +1,22 @@
+# Bienvenido a Faraw, este concepto de sistema operativo textual desarrollado en el lenguaje python, este archivo
+# es el main, aca es donde la estructura principal de Faraw se desarrolla.
+#
+# Si necesitas aplicaciones como calculadora, ve a el directorio /appsx/releases/.
+# este es uno de los archivos que mas se actualize cada vez que se agregue una nueva app.
+
+import time
+import urllib.request
+import os
+import sys
+if len(sys.argv) > 1:
+    orden = sys.argv[1]
+    if orden == "skip":
+        os.system("python3 ./startup.py")
+    elif orden == "abort":
+        os.abort
+else:
+    time.sleep(1)
+# url_a_descargar = 
+# nombre = 
+# urllib.request.urlretrieve(url=url_a_descargar, filename=nombre)
+    os.system("python3 ./startup.py normal")
