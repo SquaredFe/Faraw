2f58e2fbfe7510a950770f1620af9bb3c0d8db82
FeFe <90733495+fefedevv@users.noreply.github.com>
2023-05-12 19:28:54 -0300

    Agregada en Revision1Version1
    Primera version estable para el uso.

--- main/mainb.py
+++ main/mainb.py
@@ -4,7 +4,6 @@
 faraw_ver = "1.0.0"
 faraw_rev = "1"
 import os
-import curses
 import time
 os.system('cls' if os.name == 'nt' else 'clear')
 print("¡Te damos la bienvenida a Faraw!")
@@ -23,12 +22,36 @@ os.system('cls' if os.name == 'nt' else 'clear')
 print("Faraw version " + faraw_ver + ", Revision " + faraw_rev)
 print("Bienvenido/a a Faraw, si necesitas ayuda, escribe 'help'. ¡Gracias por utilizar Faraw!")
 
-def dec():
+def dec(cln):
+    if cln == "true":
+        os.system('cls' if os.name == 'nt' else 'clear')
     usrinp = input("user@faraw $ ")
-    if usrinp == "":
-        print("\n")
+    if usrinp == "apps -ls":
+        print("Tienes las siguientes apps instaladas:")
+        if os.path.exists(subdirectory + "/appsx/releases/calculate"):
+            print("Calculadora de Faraw (abrir con 'apps calculadora')")
+            dec("")
     elif usrinp == "help":
         print("¡Bienvenido/a a Faraw, un concepto de sistema operativo textual, aqui te ofrecemos los distintos comandos posibles:")
         print("print [input]           Descripcion: Hace que se escriba un mensaje en la terminal")
         print("apps  [-ls] [nombre de app]  Descripcion: Ejecuta una aplicacion, el argumento 'ls' muestra la lista de apps") 
         print("                             ejemplo: apps calculadora: Abre la calculadora.")
+        print("config                       Descripcion: Abre la configuracion (si esta disponible en /appsx)")
+        input("Enter para cerrar...")
+        dec(cln="true")
+    elif usrinp.__contains__("apps") == True:
+        if usrinp.__contains__("calculadora") == True:
+         os.system("python3 " + subdirectory + "/appsx/releases/calculate/main/main.py")
+         print("Volviendo a Faraw...")
+         time.sleep(2)
+         dec("true")
+        else:
+            print("Verifica el comando e intentalo nuevamente")
+            dec("")
+    elif usrinp == "exit":
+        print("Saliendo de Faraw...")
+        time.sleep(2)
+    else:
+        print(usrinp + ": not found[1]")
+        dec(cln="")
+dec(cln="")
