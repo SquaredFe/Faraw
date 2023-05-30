ef858c222a58326f8eb301f5c999d952db21b641
FeFe <90733495+fefedevv@users.noreply.github.com>
2023-05-30 11:41:52 -0300

    Progreso de la revision 2
    30 de mayo del 2023

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Si necesitas que Faraw saltee las actualizaciones, ejecuta main.py de esta manera:
 # 'python3 ./main.py skip'
 
+# Revision 2 0523
 import time
 import urllib.request
 import urllib.error
@@ -12,15 +13,9 @@ import zipfile
 import requests
 import json
 # Hacer una solicitud GET a la API de GitHub
-response = requests.get("https://api.github.com/repos/fefedevv/FarawCalculate/releases/latest")
-
-# Analizar la respuesta JSON
+response = requests.get("https://api.github.com/repos/fefedevv/FarawCalculate/releases/latest") 
 data = response.json()
-
-# Obtener el nombre del último release
 latest_release = data["name"]
-
-
 if len(sys.argv) > 1:
     orden = sys.argv[1]
     # Si la orden es "skip" el sistema saltara automaticamente la descarga.
@@ -30,14 +25,8 @@ if len(sys.argv) > 1:
         os.abort
 else:
     time.sleep(1)
-# Calculate
-# URL de la API de GitHub para obtener información de los releases
 api_url = "https://api.github.com/repos/fefedevv/FarawCalculate/releases"
-
-# Hacer una solicitud GET a la API de GitHub
 response = urllib.request.urlopen(api_url)
-
-# Analizar la respuesta JSON
 data = json.load(response)
 
 # Obtener la última versión del release
@@ -62,5 +51,53 @@ with zipfile.ZipFile(nombre, 'r') as zip_ref:
 
 if os.path.exists(destination_dir):
     print("La aplicacion Calculadora de Faraw ya existe o ya se instalo, saltando...")
+print("Transladado a main nuevamente, saliendo...")
+
+api_url = "https://api.github.com/repos/fefedevv/FarawNeofetch/releases"
+response2 = urllib.request.urlopen(api_url)
+data2 = json.load(response2)
+latest_release2 = data2[0]["tag_name"]
+url_a_descargar2 = data2[0]["assets"][0]["browser_download_url"]
+nombre2 = "neofetch.zip"
+destination_dir2 = "./appsx/releases/neofetch"
+dexs2 = "./appsx"
+
+urllib.request.urlretrieve(url=url_a_descargar2, filename=nombre2)
+
+with zipfile.ZipFile(nombre2, 'r') as zip_ref:
+    os.chmod(dexs2, 0o777)
+    try: 
+        os.makedirs(destination_dir2)
+    except FileExistsError or urllib.error.URLError as e:
+        print("La aplicacion Neofetch ya existe o ya se instalo, saltando...")
+        print("Es posible que debas de conectarte a internet, tal vez el error se deba a que no se puede conectar a los servidores de GitHub...")
+    zip_ref.extractall(destination_dir2)
+
+if os.path.exists(destination_dir2):
+    print("La aplicacion Neofetch ya existe o ya se instalo, saltando...")
+
+# Faraw Calendar
+api_url = "https://api.github.com/repos/fefedevv/FarawCalendar/releases"
+response2 = urllib.request.urlopen(api_url)
+data2 = json.load(response2)
+latest_release2 = data2[0]["tag_name"]
+url_a_descargar2 = data2[0]["assets"][0]["browser_download_url"]
+nombre2 = "calendar.zip"
+destination_dir2 = "./appsx/releases/FarawCalendar"
+dexs2 = "./appsx"
+
+urllib.request.urlretrieve(url=url_a_descargar2, filename=nombre2)
+
+with zipfile.ZipFile(nombre2, 'r') as zip_ref:
+    os.chmod(dexs2, 0o777)
+    try: 
+        os.makedirs(destination_dir2)
+    except FileExistsError or urllib.error.URLError as e:
+        print("La aplicacion Calendar ya existe o ya se instalo, saltando...")
+        print("Es posible que debas de conectarte a internet, tal vez el error se deba a que no se puede conectar a los servidores de GitHub...")
+    zip_ref.extractall(destination_dir2)
+
+if os.path.exists(destination_dir2):
+    print("La aplicacion Calendar ya existe o ya se instalo, saltando...")
 os.system("python3 ./main/mainb.py normal")
 print("Transladado a main nuevamente, saliendo...")
--- main/mainb.py
+++ main/mainb.py
@@ -2,7 +2,7 @@
 # Faraw Calculate o Faraw Games
 # Estructura del programa // import
 faraw_ver = "1.0.0"
-faraw_rev = "1"
+faraw_rev = "2"
 import os
 import time
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -29,29 +29,46 @@ def dec(cln):
     if usrinp == "apps -ls":
         print("Tienes las siguientes apps instaladas:")
         if os.path.exists(subdirectory + "/appsx/releases/calculate"):
-            print("Calculadora de Faraw (abrir con 'apps calculadora')")
-            dec("")
+            print("  Calculadora de Faraw (abrir con 'apps calculadora')")
+            
+        if os.path.exists(subdirectory + "/appsx/releases/neofetch/Faraw-Neofetch/"):
+            print("  Faraw Neofetch (abrir con 'apps neofetch')")
+        dec("")
     elif usrinp == "help":
         print("¡Bienvenido/a a Faraw, un concepto de sistema operativo textual, aqui te ofrecemos los distintos comandos posibles:")
         print("print [input]           Descripcion: Hace que se escriba un mensaje en la terminal")
         print("apps  [-ls] [nombre de app]  Descripcion: Ejecuta una aplicacion, el argumento 'ls' muestra la lista de apps") 
         print("                             ejemplo: apps calculadora: Abre la calculadora.")
-        print("config                       Descripcion: Abre la configuracion (si esta disponible en /appsx)")
+        # print("config                       Descripcion: Abre la configuracion (si esta disponible en /appsx)")
         input("Enter para cerrar...")
         dec(cln="true")
+    
     elif usrinp.__contains__("apps") == True:
         if usrinp.__contains__("calculadora") == True:
          os.system("python3 " + subdirectory + "/appsx/releases/calculate/main/main.py")
          print("Volviendo a Faraw...")
          time.sleep(2)
          dec("true")
+        elif usrinp.__contains__("neofetch") == True:
+            os.system("python3 " + subdirectory + "/appsx/releases/neofetch/Faraw-Neofetch/main/main.py")
+            print("Volviendo a Faraw...")
+            time.sleep(2)
+            dec("true")
+        elif usrinp.__contains__("calendar") == True:
+            os.system("python3 " + subdirectory + "/appsx/releases/neofetch/FarawCalendar/main/main.py")
+            print("Volviendo a Faraw...")
+            time.sleep(2)
+            dec("true")
         else:
             print("Verifica el comando e intentalo nuevamente")
             dec("")
     elif usrinp == "exit":
         print("Saliendo de Faraw...")
         time.sleep(2)
+    elif usrinp.__contains__("print") == True:
+        print(usrinp.replace("print ", ""))
+        dec("")
     else:
-        print(usrinp + ": not found[1]")
+        print(usrinp + ": Orden o funcion no encontrada (¿Perdido? Utiliza la funcion integrada help\n para ver todas las funciones y programas disponibles.)")
         dec(cln="")
 dec(cln="")
